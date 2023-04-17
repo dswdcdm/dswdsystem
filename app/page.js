27fadf3176310d39e99da11d798ed72dd672d20e
dswdcdm <dswdcdm@gmail.com>
2023-04-17 11:09:26 +0800

    update

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import MainContent from "./MainContent";
 
 export default function Home() {
   return (
    <main>
     <Navbar/>
-      
+      <MainContent/>
     <Footer/>
     </main>
   )

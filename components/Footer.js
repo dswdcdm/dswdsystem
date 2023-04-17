4627b6052af2d84d296548e2c69bf962cd794c76
dswdcdm <dswdcdm@gmail.com>
2023-04-17 10:18:24 +0800

    footer

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div><div className='w-full p-6 bg-gray-300 text-white text-center'><span>&copy;copyright 2023 -2023</span></div></div>
+  )
+}
+
+export default Footer
---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="w-full mx-auto bg-gray-300 py-6">
+      <div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
